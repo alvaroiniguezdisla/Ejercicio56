e9435f44d365f6e95a9e8534f01d34de91ac9d30
Álvaro <alvaro.iniguezdisla@usp.ceu.es>
2023-04-30 18:07:53 +0200

    ejercicio56

--- src/main/java/org/example/ejercicio56.java
+++ src/main/java/org/example/ejercicio56.java
@@ -1,2 +1,15 @@
-package org.example;public class ejercicio56 {
+package org.example;
+import java.math.BigDecimal;
+import java.util.Stack;
+
+public class ejercicio56 {
+    static public BigDecimal sumaRecursivaElementosPila(Stack pila){//complejidad O(n)
+        BigDecimal suma = BigDecimal.ZERO;
+        if(pila.empty()){
+            return BigDecimal.ZERO;
+        }else{
+         return suma.add((BigDecimal) pila.pop()).add(sumaRecursivaElementosPila(pila));
+
+        }
+    }
 }
--- src/test/java/org/example/test_ej56.java
+++ src/test/java/org/example/test_ej56.java
@@ -1,2 +1,25 @@
-package org.example;public class test_ej56 {
+package org.example;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.math.BigDecimal;
+import java.util.Stack;
+
+public class test_ej56 {
+    @Test
+    public void testSumaRecursivaElementosPila() {
+        Stack pila = new Stack();
+        pila.push(1);
+        pila.push(2);
+        pila.push(3);
+        pila.push(4);
+        pila.push(5);
+        pila.push(6);
+        pila.push(7);
+        assertEquals(new BigDecimal(28), ejercicio56.sumaRecursivaElementosPila(pila));
+    }
+    @Test
+    void testSumaRecursivaElementosPila0() {
+        Stack pila = new Stack();
+        assertEquals(BigDecimal.ZERO, ejercicio56.sumaRecursivaElementosPila(pila));
+    }
 }
